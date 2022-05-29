0ebd13a9d4bb4942b6f6d21ac41dd82eb272c1ec
Joe Frazier <54604023+jfrazier-eth@users.noreply.github.com>
2022-05-29 16:36:21 -0500

    get tracks for a playlist

---
+++ src/getPlaylistTracks.R
@@ -0,0 +1,35 @@
+install.packages("Rspotify", repos = "http://cran.wustl.edu/")
+library("Rspotify")
+
+get_playlist_tracks <- function(user_token, owner_id, playlist_id) {
+    page_size <- 100
+    has_next_page <- TRUE
+    offset <- 0
+    tracks <- data.frame()
+
+    while (has_next_page) {
+        page <- getPlaylistSongs(owner_id, playlist_id, offset, user_token)
+        index <- 1
+        for (track_name in page$tracks) {
+            track <- data.frame(
+                track_name = track_name,
+                album_id = page$album_id[index],
+                track_id = page$id[index],
+                popularity = page$popularity[index],
+                artist = page$artist[index],
+                artist_id = page$artist_id[index],
+                album = page$album[index]
+            )
+            if (length(tracks) == 0) {
+                tracks <- track
+            } else {
+                tracks <- rbind(tracks, track)
+            }
+            index <- index + 1
+        }
+
+        has_next_page <- length(page$tracks) == page_size
+        offset <- dim(tracks)[1]
+    }
+    return(tracks)
+}
--- src/main.R
+++ src/main.R
@@ -1,11 +1,12 @@
 source("./src/oauth.R")
 source("./src/selectPlaylist.R")
+source("./src/getPlaylistTracks.R")
 
 # create a function that prints hello world
 main <- function() {
   user_token <- get_token()
 
-  user_id <- readline(prompt="Enter your spotify username: ")
+  user_id <- readline(prompt = "Enter your spotify username: ")
 
   user <- getUser(user_id = user_id, token = user_token)
   display_name <- user$display_name
@@ -13,9 +14,19 @@ main <- function() {
   print(paste("Welcome", display_name))
 
   selected_playlist <- select_playlist(
-    user_id = user_id, user_token = user_token)
+    user_id = user_id, user_token = user_token
+  )
 
   print(selected_playlist)
+
+  tracks <- get_playlist_tracks(
+    user_token = user_token,
+    owner_id = selected_playlist$owner_id, playlist_id = selected_playlist$id
+  )
+
+  print(str(tracks))
+
+
 }
 
 
